68a5fd1ccf49a2649dd34a1b7e906dbe5e139761
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-03-03 00:00:54 -0300

    Commit

---
+++ Assets/Scripts/Systems/MovableSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MovableSystem : SystemBase {
+
+    protected override void OnUpdate() {
+
+        float deltaTime = Time.DeltaTime;
+
+        Entities.ForEach((ref Movable movable, ref Translation translation, ref Rotation rotation) => {
+
+            translation.Value += movable.speed * movable.direction * deltaTime;
+            rotation.Value = math.mul(rotation.Value.value, quaternion.RotateY(movable.speed * deltaTime));
+
+        }).Schedule();
+    }
+}
