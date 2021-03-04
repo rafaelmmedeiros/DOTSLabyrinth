aae765381b5cd5575e6b722415ea746e725a3dca
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-03-04 19:48:01 -0300

    Commit

---
+++ Assets/Scripts/Systems/CollisionSystem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class CollisionSystem : SystemBase {
+    
+    protected override void OnUpdate() {
+        throw new System.NotImplementedException();
+    }
+}
