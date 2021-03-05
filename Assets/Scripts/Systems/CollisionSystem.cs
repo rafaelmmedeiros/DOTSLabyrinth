b14ab595644ca4b502ef6e85cb1be304ff2209c7
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-03-04 22:14:45 -0300

    Commit

--- Assets/Scripts/Systems/CollisionSystem.cs
+++ Assets/Scripts/Systems/CollisionSystem.cs
@@ -1,11 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
+using Unity.Physics;
 using UnityEngine;
+using Unity.Physics.Systems;
 
 public class CollisionSystem : SystemBase {
-    
+
+    private struct CollisionSystemJob : ICollisionEventsJob {
+
+        public BufferFromEntity<CollisionBuffer> collisions;
+
+        public void Execute(CollisionEvent collisionEvent) {
+
+            if (collisions.Exists(collisionEvent.EntityA))
+                collisions[collisionEvent.EntityA].Add(new CollisionBuffer() { entity = collisionEvent.EntityB });
+
+            if (collisions.Exists(collisionEvent.EntityB))
+                collisions[collisionEvent.EntityB].Add(new CollisionBuffer() { entity = collisionEvent.EntityA });
+        }
+    }
+
+    private struct TriggerSystemJob : ITriggerEventsJob {
+
+        public BufferFromEntity<TriggerBuffer> triggers;
+
+        public void Execute(TriggerEvent triggerEvent) {
+
+            if (triggers.Exists(triggerEvent.EntityA))
+                triggers[triggerEvent.EntityA].Add(new TriggerBuffer() { entity = triggerEvent.EntityB });
+
+            if (triggers.Exists(triggerEvent.EntityB))
+                triggers[triggerEvent.EntityB].Add(new TriggerBuffer() { entity = triggerEvent.EntityA });
+        }
+    }
+
     protected override void OnUpdate() {
-        throw new System.NotImplementedException();
+
+        var physicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>().PhysicsWorld;
+        var simulation = World.GetOrCreateSystem<StepPhysicsWorld>().Simulation;
+
+        Entities.ForEach((DynamicBuffer<CollisionBuffer> collisions) => {
+
+            collisions.Clear();
+
+        }).Run();
+
+        var collisionJobHandle = new CollisionSystemJob() {
+
+            collisions = GetBufferFromEntity<CollisionBuffer>()
+
+        }.Schedule(simulation, ref physicsWorld, this.Dependency);
+
+        collisionJobHandle.Complete();
+
+
+
+
+        Entities.ForEach((DynamicBuffer<TriggerBuffer> triggers) => {
+
+            triggers.Clear();
+
+        }).Run();
+
+        var triggerJobHandle = new TriggerSystemJob() {
+
+            triggers = GetBufferFromEntity<TriggerBuffer>()
+
+        }.Schedule(simulation, ref physicsWorld, this.Dependency);
+
+        triggerJobHandle.Complete();
     }
 }
