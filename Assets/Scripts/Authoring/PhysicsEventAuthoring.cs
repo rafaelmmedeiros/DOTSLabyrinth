a80536d47e21450e5cefde852a7cc202ba4244d5
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-03-07 17:52:59 -0300

    Commit

--- Assets/Scripts/Authoring/PhysicsEventAuthoring.cs
+++ Assets/Scripts/Authoring/PhysicsEventAuthoring.cs
@@ -5,23 +5,13 @@ using UnityEngine;
 [DisallowMultipleComponent]
 [RequiresEntityConversion]
 public class PhysicsEventAuthoring : MonoBehaviour, IConvertGameObjectToEntity {
-    // Add fields to your component here. Remember that:
-    //
-    // * The purpose of this class is to store data for authoring purposes - it is not for use while the game is
-    //   running.
-    // 
-    // * Traditional Unity serialization rules apply: fields must be public or marked with [SerializeField], and
-    //   must be one of the supported types.
-    //
-    // For example,
-    //    public float scale;
+
     public float health;
-
-
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
 
-
+        dstManager.AddBuffer<CollisionBuffer>(entity);
+        dstManager.AddBuffer<TriggerBuffer>(entity);
 
     }
 }
